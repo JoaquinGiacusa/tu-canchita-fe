f0f2f239a03246801a65923d1d265c38d8e7bab1
JoaquinGiacusa <joaquingiacusa.dev@gmail.com>
2022-08-17 15:39:50 -0300

    create text-1

--- components/login/index.tsx
+++ components/login/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import { Input } from "../../ui/input";
+import { Input } from "../../ui/Input";
 
 // function Input({ label, register, required }: any) {
 //   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { Text1 } from "../ui/Text1";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Text1>Uñaso</Text1>
+    </div>
+  );
 };
 
 export default Home;
