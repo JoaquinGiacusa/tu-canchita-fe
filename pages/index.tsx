95418cacec676dbc2e4be58054833842f2240c7e
JuanDls01 <juanignaciodelossantos01@gmail.com>
2022-08-17 15:40:59 -0300

    agregando Text2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { Text2 } from "../ui/Text2";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return <div>
+    <Text2 text="Hola como andas?"/>
+  </div>;
 };
 
 export default Home;
