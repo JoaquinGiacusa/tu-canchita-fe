db3ac6632cd0eb2358d4c5f9f5b124c6a3ad6e17
JoaquinGiacusa <joaquingiacusa.dev@gmail.com>
2022-08-19 13:15:46 -0300

    fix required form

--- components/login/index.tsx
+++ components/login/index.tsx
@@ -1,24 +1,38 @@
 import React from "react";
-
+import { useForm } from "react-hook-form";
 import { FormBttn } from "../../ui/FormBttn";
-import { Input } from "../../ui/Input";
+import { Input } from "../../ui/input";
 import { Text1 } from "../../ui/Text1";
 
 export const Form = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = (data: any) => console.log(data);
+
   return (
-    <div className="flex flex-col max-w-md px-4 py-8 text-center bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10">
+    <div className="flex flex-col max-w-md px-4 py-8 mx-auto text-center bg-white rounded-lg shadow justify-self-center dark:bg-gray-800 ">
       <Text1>Ingresar</Text1>
 
-      <div className="p-6 mt-8">
-        <form action="#" className="flex flex-col gap-3">
-          <Input name="email" type="text" placeHolder="Email"></Input>
+      <div className="p-6">
+        <form className="flex flex-col gap-3" onSubmit={handleSubmit(onSubmit)}>
           <Input
-            name="password"
+            type="email"
+            placeHolder="Email"
+            register={register}
+            required={true}
+          ></Input>
+          <Input
             type="password"
             placeHolder="Contraseña"
+            register={register}
+            required={true}
           ></Input>
 
-          <FormBttn placeholder="Crear cuenta"></FormBttn>
+          <FormBttn placeholder="Ingresar"></FormBttn>
         </form>
       </div>
     </div>
--- ui/input.tsx
+++ ui/input.tsx
@@ -1,16 +1,19 @@
 type Input = {
-  name: string;
   type: string;
   placeHolder: string;
+  register: any;
+  required: boolean;
 };
 
-export const Input = ({ name, type, placeHolder }: Input) => {
+export const Input = ({ type, placeHolder, register, required }: Input) => {
+  console.log("xd", required);
+
   return (
     <input
-      name={name}
       type={type}
       className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-transparent border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
       placeholder={placeHolder}
+      {...register(type, { required })}
     />
   );
 };
